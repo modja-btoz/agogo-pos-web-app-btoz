10b35817fc09be14fedb07f45a0ddd10cb1b8912
Endra Modja Indriyanto <modja@Endras-MacBook-Air.local>
2019-04-10 12:38:31 +0700

    fix conflict after merged with branch putra

--- src/components/navigations/FooterNavRight.jsx
+++ src/components/navigations/FooterNavRight.jsx
@@ -7,17 +7,10 @@ const FooterNavRight = (props) => {
     <Navbar expand="md">
       <Nav className="ml-auto" navbar>
         <NavItem>
-<<<<<<< HEAD
           <NavLink active={props.cartStore.state.isOpenRefundShow} onClick={() => props.cartStore.openRefund()}><i className="fas fa-retweet"></i><br />Refund</NavLink>
         </NavItem>
         <NavItem>
           <NavLink active={props.cartStore.state.isOpenReservationShow} onClick={() => props.cartStore.openReservation()}><i className="fas fa-edit"></i> <br />Pemesanan</NavLink>
-=======
-          <NavLink href="/refund"><i className="fas fa-retweet"></i><br />Refund</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/pemesanan"><i className="fas fa-edit"></i> <br />Pemesanan</NavLink>
->>>>>>> 900f054379670bed1524833a7ac06c01b1e3dd3c
         </NavItem>
         <NavItem>
           <NavLink href="/logout"><i className="fas fa-sign-out-alt"></i> <br />Sign Out</NavLink>
